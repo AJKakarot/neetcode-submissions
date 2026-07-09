f6e94c352121fa54dd68e886283736a037ad199c
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-07-09 16:24:59 +0530

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,47 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else if (minStack.peek() >= val) {
+            minStack.push(val);
+        } else {
+            stack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (minStack.peek() == stack.peek()) {
+            minStack.pop();
+            stack.pop();
+
+        } else {
+            stack.pop();
+        }
+    }
+
+    public int top() {
+
+if(stack.isEmpty()){
+    return null;
+}
+
+       return stack.peek();
+    }
+
+    public int getMin() {
+
+if(minStack.isEmpty()){
+    return null;
+}
+
+       return minStack.peek();
+    }
+}
