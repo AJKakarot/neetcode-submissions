ec0e54004343d9bc029a445ac445cd9acb1643f9
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-07-11 03:00:46 +0530

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+class StockSpanner {
+
+    // Stack stores [price, span]
+    Stack<int[]> stack;
+
+    // Constructor
+    public StockSpanner() {
+
+        System.out.println("Constructor Called");
+
+        stack = new Stack<>();
+    }
+
+    // Returns span of current stock price
+    public int next(int price) {
+
+        int span = 1;
+
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+
+            span += stack.pop()[1];
+
+        }
+
+        stack.push(new int[] { price, span });
+
+        return span;
+    }
+}
+
+// public class Main {
+
+//     public static void main(String[] args) {
+
+//         // Object Creation
+//         StockSpanner sp = new StockSpanner();
+
+//         System.out.println(sp.next(100));
+//         System.out.println(sp.next(80));
+//         System.out.println(sp.next(60));
+//         System.out.println(sp.next(70));
+//         System.out.println(sp.next(60));
+//         System.out.println(sp.next(75));
+//         System.out.println(sp.next(85));
+//     }
+// }
