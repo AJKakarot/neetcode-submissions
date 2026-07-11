96c9b01a38c071c545f72fb300793cfdd30f9b27
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-07-12 03:36:28 +0530

    Add: guess-number-higher-or-lower - submission-4

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-4.java
@@ -0,0 +1,32 @@
+public class Solution extends GuessGame {
+
+    public int guessNumber(int n) {
+
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int result = guess(mid);
+
+            if (result == 0) {
+
+                return mid;
+
+            } else if (result == -1) {
+
+                // Guess is too high
+                right = mid - 1;
+
+            } else {
+
+                // Guess is too low
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
