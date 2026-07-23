bf40307e3fab765bc4d402f955ffd4092401d572
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-07-23 07:00:37 +0530

    Add: construct-quad-tree - submission-2

---
+++ Data Structures & Algorithms/construct-quad-tree/submission-2.java
@@ -0,0 +1,89 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+
+    public Node construct(int[][] grid) {
+        return build(grid, 0, 0, grid.length);
+    }
+
+    private Node build(int[][] grid, int row, int col, int size) {
+
+        // Check if all values in this square are same
+        boolean same = true;
+        int value = grid[row][col];
+
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (grid[i][j] != value) {
+                    same = false;
+                    break;
+                }
+            }
+            if (!same) break;
+        }
+
+        // Base Case: All values are same
+        if (same) {
+            return new Node(value == 1, true);
+        }
+
+        // Divide into 4 parts
+        int half = size / 2;
+
+        Node topLeft = build(grid, row, col, half);
+
+        Node topRight = build(grid, row, col + half, half);
+
+        Node bottomLeft = build(grid, row + half, col, half);
+
+        Node bottomRight = build(grid, row + half, col + half, half);
+
+        // Internal Node
+        return new Node(
+            true,          // value can be true/false, ignored for internal nodes
+            false,         // isLeaf = false
+            topLeft,
+            topRight,
+            bottomLeft,
+            bottomRight
+        );
+    }
+}
