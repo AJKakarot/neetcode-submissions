85ed345b6ba09b49096c04fb87293ee60921af2d
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-08-03 06:35:34 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    private void solve(int[] nums,
+                       boolean[] used,
+                       List<Integer> temp,
+                       List<List<Integer>> result) {
+
+        // Base Case
+        if (temp.size() == nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        // Try every unused element
+        for (int i = 0; i < nums.length; i++) {
+
+            // Skip if already used
+            if (used[i]) {
+                continue;
+            }
+
+            // Choose
+            used[i] = true;
+            temp.add(nums[i]);
+
+            // Explore
+            solve(nums, used, temp, result);
+
+            // Backtrack (Undo)
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        solve(nums, used, temp, result);
+
+        return result;
+    }
+}
