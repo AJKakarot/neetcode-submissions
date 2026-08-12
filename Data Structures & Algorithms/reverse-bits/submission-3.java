4b6eca74a7a64684be16082a17933408db9f794a
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-08-12 07:10:08 +0530

    Add: reverse-bits - submission-3

---
+++ Data Structures & Algorithms/reverse-bits/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        // Exactly 32 bits process karni hain
+        for (int i = 0; i < 32; i++) {
+            // n ka last bit nikalo
+            int bit = n & 1;
+
+            // Result mein space banao
+            result = result << 1;
+
+            // Nikali hui bit ko result mein add karo
+            result = result | bit;
+
+            // n ki next bit par jao
+            n = n >> 1;
+        }
+
+        return result;
+    }
+}
