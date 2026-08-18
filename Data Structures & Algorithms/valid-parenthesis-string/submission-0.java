72cebe23cd40c614bdf73e5766a6002ab14eabee
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-08-18 09:05:10 +0530

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.java
@@ -0,0 +1,47 @@
+class Solution {
+    public :
+
+        bool
+        solve(int idx, int open, string& s, int n) {
+        // String finished
+        if (idx == n) {
+            return open == 0;
+        }
+
+        bool isValid = false;
+
+        // '*'
+        if (s[idx] == '*') {
+            // '*' = '('
+            isValid |= solve(idx + 1, open + 1, s, n);
+
+            // '*' = ''
+            isValid |= solve(idx + 1, open, s, n);
+
+            // '*' = ')'
+            if (open > 0) {
+                isValid |= solve(idx + 1, open - 1, s, n);
+            }
+        }
+
+        // '('
+        else if (s[idx] == '(') {
+            isValid |= solve(idx + 1, open + 1, s, n);
+        }
+
+        // ')'
+        else {
+            if (open > 0) {
+                isValid |= solve(idx + 1, open - 1, s, n);
+            }
+        }
+
+        return isValid;
+    }
+
+    bool checkValidString(string s) {
+        int n = s.length();
+
+        return solve(0, 0, s, n);
+    }
+};
