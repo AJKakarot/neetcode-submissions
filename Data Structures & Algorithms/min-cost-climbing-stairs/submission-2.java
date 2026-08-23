4e6b6e2743b3096eda8fde8d0d8c4403d38cf744
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-08-23 14:03:06 +0530

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.java
@@ -0,0 +1,51 @@
+
+class Solution {
+    int[] dp;
+
+    int solve(int[] cost, int i) {
+        if (i < 0) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        return dp[i] = cost[i] + Math.min(solve(cost, i - 1), solve(cost, i - 2));
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return Math.min(solve(cost, n - 1), solve(cost, n - 2));
+    }
+}
+
+// class Solution {
+
+//     public int minCostClimbingStairs(int[] cost) {
+
+//         int n = cost.length;
+
+//         int[] dp = new int[n];
+
+//         dp[0] = cost[0];
+//         dp[1] = cost[1];
+
+//         for (int i = 2; i < n; i++) {
+
+//             dp[i] = cost[i] + Math.min(
+//                 dp[i - 1],
+//                 dp[i - 2]
+//             );
+//         }
+
+//         return Math.min(
+//             dp[n - 1],
+//             dp[n - 2]
+//         );
+//     }
+// }
