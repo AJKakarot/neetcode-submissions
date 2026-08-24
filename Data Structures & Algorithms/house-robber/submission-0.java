810b8a4ced2572b577066c4edab594901cb2f5a8
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-08-25 01:20:05 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solve(int[] nums, int i, int[] dp) {
+        // Base case
+        if (i >= nums.length) {
+            return 0;
+        }
+
+        // Already calculated
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        // Take
+        int take = nums[i] + solve(nums, i + 2, dp);
+
+        // Skip
+        int skip = solve(nums, i + 1, dp);
+
+        // Store answer
+        dp[i] = Math.max(take, skip);
+
+        return dp[i];
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return solve(nums, 0, dp);
+    }
+}
