f8d885400315dfc516edc6f5dfd9bce065d4604f
Ajeet Gupta <140450935+AJKakarot@users.noreply.github.com>
2026-09-26 04:36:52 +0530

    Add: integer-break - submission-1

---
+++ Data Structures & Algorithms/integer-break/submission-1.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+class Solution {
+
+    private int[] t;
+
+    private int solve(int n) {
+
+        // Base case
+        if (n == 1) {
+            return 1;
+        }
+
+        // Already calculated
+        if (t[n] != -1) {
+            return t[n];
+        }
+
+        int result = Integer.MIN_VALUE;
+
+        // Break n into i + (n-i)
+        for (int i = 1; i < n; i++) {
+
+            int product = i * Math.max(
+                n - i,
+                solve(n - i)
+            );
+
+            result = Math.max(result, product);
+        }
+
+        // Store and return
+        return t[n] = result;
+    }
+
+    public int integerBreak(int n) {
+
+        t = new int[n + 1];
+        Arrays.fill(t, -1);
+
+        return solve(n);
+    }
+}
